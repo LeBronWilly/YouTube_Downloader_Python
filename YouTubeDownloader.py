1b2a271a3f1e963e3e50f7d02d1e1be98b36cfe9
Willy Fang <w70024@gmail.com>
2022-08-06 21:42:35 +0800

    Modify UI

--- YouTubeDownloader.py
+++ YouTubeDownloader.py
@@ -1,9 +1,14 @@
+# https://ithelp.ithome.com.tw/users/20140047/ironman/4481
+# https://vocus.cc/article/624e6738fd89780001ae7c1e
+# https://ithelp.ithome.com.tw/articles/10278264
+# https://www.delftstack.com/zh-tw/tutorial/tkinter-tutorial/tkinter-geometry-managers/#tkinter-grid-%25E4%25BD%2588%25E5%25B1%2580%25E6%2596%25B9%25E6%25B3%2595
+
 from Willy_Modules import *
 
 
 import tkinter as tk
 from pytube import YouTube
-def clickUrl():  # 按「確定」鈕後處理函式
+def clickUrl():  # 按「確定」鈕後處理函式 (btnUrl)
     global list_video, list_radio, yt
     for r in list_radio:  # 移除選項按鈕
         r.destroy()
@@ -25,6 +30,7 @@ def clickUrl():  # 按「確定」鈕後處理函式
             for v1 in yt.streams.filter(progressive=True).all():  # 建立影片格式串列
                 list_video.append(v1)
             for v2 in list_video:  # 迴圈建立多個影片格式選項按鈕
+                # 這個元件就是常見的按圓形按紐，通常不能複選。
                 rbtem = tk.Radiobutton(frame3, text=v2,
                                        variable=video,
                                        value=rbvalue,
@@ -104,46 +110,57 @@ def cd():  # 按「下載音樂」鈕後處理函式
 
 
 
-win = tk.Tk()
-win.geometry("600x400")  # 設定主視窗解析度
-win.title("YouTube_Downloader_Python")
+win = tk.Tk() # GUI的核心，需要用這個函式建立架構
+win.title("YouTube_Downloader_Python") #
+win.geometry("600x400")  # 設定主視窗解析度(長寬設定)
 
 get_video = ""  # 影片格式
 str_ftype = ""  # 影片型態
 list_video = []  # 影片格式串列
 list_radio = []  # 選項按鈕串列
 video = tk.IntVar()  # 選項按鈕值
-url = tk.StringVar()  # 影片網址
-path = tk.StringVar()  # 存檔資料夾
-filename = tk.StringVar()  # 存檔名稱
+url = tk.StringVar()  # 影片網址 (entry_Url)
+path = tk.StringVar()  # 存檔資料夾 (entry_Path)
+filename = tk.StringVar()  # 存檔名稱 (entry_File)
 
 
 frame1 = tk.Frame(win, width=450)
-frame1.pack()
-label1 = tk.Label(frame1, text="YouTube網址：")
+# 在tkinter中，有三種佈局，分別是pack、grid以及place
+frame1.pack(side="top")  # 其中pack為最基礎的佈局方式
+
+# 可以想像成是一個空的盒子，可以放入你想要的東西
+# 如果要使用image函式來塞圖片，先用tk.PhotoImage()這個功能選取圖片後，再丟到Label中
+label1 = tk.Label(frame1, text="YouTube Url: ")
+# Entry用來呈現讓使用者輸入文字的視窗，利用show()函式可以將輸入的文字轉成指定的文字
 entry_Url = tk.Entry(frame1, textvariable=url)
 entry_Url.config(width=50)
-
-btnUrl = tk.Button(frame1, text="爬取！", command=clickUrl)
-label1.grid(row=0, column=0, sticky="e")
+# button這個元件就是按鈕，比較重要的參數就是text，用來顯示按鈕內的文字。
+btnUrl = tk.Button(frame1, text="Scrape!", command=clickUrl)
+# grid可以想像成是表格式的排列方法，可以利用控制row(列)以及column(行)來有規律地規劃元素
+label1.grid(row=0, column=0, pady=10, sticky="e")
 entry_Url.grid(row=0, column=1)
 btnUrl.grid(row=0, column=2)
 
-label2 = tk.Label(frame1, text="存檔路徑資料夾：")
+
+label2 = tk.Label(frame1, text="Folder Path to Save File: ")
 entry_Path = tk.Entry(frame1, textvariable=path)
-entry_Path.config(width=40)
-label2.grid(row=1, column=0, pady=6, sticky="e")
+entry_Path.config(width=50)
+label2.grid(row=1, column=0, pady=10, sticky="e")
 entry_Path.grid(row=1, column=1)
 
-label3 = tk.Label(frame1, text="名稱顯示：")
+
+label3 = tk.Label(frame1, text="File Name: ")
 entry_File = tk.Entry(frame1, textvariable=filename)
-entry_File.config(width=40)
-label3.grid(row=2, column=0, pady=3, sticky="e")
+entry_File.config(width=50)
+label3.grid(row=2, column=0, pady=10, sticky="e")
 entry_File.grid(row=2, column=1)
+
 
 label4 = tk.Label(frame1, fg="blue", font=(12),
                 text="下載速度視電腦而定，音樂會稍慢些\n程式會停頓代表在執行中，並非當掉！\n")
 label4.grid(row=3, column=1, columnspan=2,sticky="se")
+
+
 
 
 frame2 = tk.Frame(win)
@@ -162,4 +179,4 @@ labelMsg.pack()
 frame3 = tk.Frame(win)  # 選項按鈕區塊
 frame3.pack()
     
-win.mainloop()
+win.mainloop() # 非常重要的函式，會使程式常駐執行
